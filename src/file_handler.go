99b0785dc3a0fdd6d22a65ad2776ed6d7430b71e
Pizio Leonardo <pizio.leonardo@gmail.com>
2021-06-14 21:10:52 +0200

    Merged addPicture and addVideo into one

--- src/file_handler.go
+++ src/file_handler.go
@@ -1,41 +1,39 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"io/ioutil"
 	"os"
 )
 
 const (
-	basePath         string = ""
-	videoExtension   string = ".mp4"
-	pictureExtension string = ".png"
+	basePath     string = "/photo_sync/users/"
+	postBasePath string = "/objects/"
 )
 
-//	Returns the path of the file, the size of the file and then the error (if any)
-func CreatePicture(picture []byte, imageName string) (string, int64, error) {
-	imagePath := basePath + imageName + pictureExtension
-	err := ioutil.WriteFile(imagePath, picture, 0644)
+func CreateObjectFile(object []byte, objectName string, objectExtension string, username string) (string, int64, error) {
+	objectPath := basePath + username + postBasePath + objectName + objectExtension
+	exists, err := FileExists(objectPath)
 	if err != nil {
+		fmt.Print(err)
 		return "", 0, err
 	}
-	info, _ := os.Stat(imagePath)
-	return imagePath, info.Size(), nil
-}
-
-//	Returns the path of the file, the size of the file and then the error (if any)
-func CreateVideo(video []byte, videoName string) (string, int64, error) {
-	videopath := basePath + videoName + pictureExtension
-	err := ioutil.WriteFile(videopath, video, 0644)
+	if exists {
+		return "", 0, errors.New("already_exists")
+	}
+	err = ioutil.WriteFile(objectPath, object, 0644)
 	if err != nil {
+		fmt.Print(err)
 		return "", 0, err
 	}
-	info, _ := os.Stat(videopath)
-	return videopath, info.Size(), nil
+	info, _ := os.Stat(objectPath)
+	return objectPath, info.Size(), nil
 }
 
 // Checks if the file already exists
-func FileExists(fileName string, fileExtension string) (bool, error) {
-	_, err := os.Stat(basePath + fileName + fileExtension)
+func FileExists(filePath string) (bool, error) {
+	_, err := os.Stat(filePath)
 	if err != nil {
 		return false, err
 	}
